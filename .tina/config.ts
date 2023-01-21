5d12ddbec800a2f13f1b31814fbd696371c24678
Guilherme Silveira <gbasilveira@gmail.com>
2023-01-21 16:47:18 +0000

    tina config

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "01e4b01b-4444-47fe-918a-0400a772b003", // Get this from tina.io
+  token: "4548e94697aa92cad68943090e3e3c07d5dbaa98", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
